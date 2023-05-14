6c0bdd210cb4fded9ecb053d1878e4fb29718fb8
jacobaranza <jacobthomasjunior@gmail.com>
2023-05-14 08:54:00 -0500

    dom modifyinfg lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// Remove the DOM node 'main#main'
+const mainNode = document.querySelector('main#main');
+mainNode.remove();
+
+// Create a new <h1> element and assign it to the 'newHeader' variable
+const newHeader = document.createElement('h1');
+
+// Set the id of the 'newHeader' element to 'victory'
+newHeader.id = 'victory';
+
+// Set the text content of the 'newHeader' element to "YOUR-NAME is the champion"
+newHeader.textContent = "Jacob Thomas is the champion";
+
+// Append the 'newHeader' element to the document body
+document.body.appendChild(newHeader);
